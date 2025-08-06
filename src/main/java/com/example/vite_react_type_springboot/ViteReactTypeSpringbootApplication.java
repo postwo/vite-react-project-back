4f1a0600e59332087cc171853c5e8601855828b2
postwo <codo7717@naver.com>
2025-08-06 15:45:08 +0900

    초기 설정

---
+++ src/main/java/com/example/vite_react_type_springboot/ViteReactTypeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.vite_react_type_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViteReactTypeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViteReactTypeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vite_react_type_springboot/ViteReactTypeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vite_react_type_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViteReactTypeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
